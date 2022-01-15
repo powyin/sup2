96c4e007b77f3b0d0358841bee6c646d8ecec39a
殷豪祥 <yinhaoxiang@supcon.com>
2022-01-15 17:14:12 +0800

    init

---
+++ src/pages/login/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="index">
+    <button>1111111</button>
+    <text class="test"> 11 </text>
+    <scroll-view>++++++++</scroll-view>
+  </view>
+</template>
+
+<script>
+import { onMounted, reactive, ref, computed } from 'vue'
+import './index.scss'
+
+// import { useStore } from 'vuex'
+
+export default {
+  name: 'test',
+  setup() {
+    // const store = useStore()
+    // const count = computed(() => store.state.userInfo.count)
+    // const res = ref([])
+    // const msg = ref('111')
+    // const getRes = async () => {
+    //   const result = await test()
+    //   res.value = result
+    // }
+    // getRes()
+    // return { res, msg }
+  }
+}
+</script>
+
